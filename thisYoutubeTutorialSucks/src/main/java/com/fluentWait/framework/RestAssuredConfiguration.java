c0d78157161ef82de9e64d9d86a8de168428039a
ZacSol <zsollecito@teksystems.com>
2019-07-11 16:43:26 -0500

    finished tutorial for day

--- thisYoutubeTutorialSucks/src/main/java/com/fluentWait/framework/RestAssuredConfiguration.java
+++ thisYoutubeTutorialSucks/src/main/java/com/fluentWait/framework/RestAssuredConfiguration.java
@@ -2,7 +2,9 @@ package com.fluentWait.framework;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 
 public class RestAssuredConfiguration {
@@ -34,4 +36,10 @@ public class RestAssuredConfiguration {
         return RestAssured.given().contentType(ContentType.JSON);
     }
 
+    public Response getResponse(RequestSpecification requestSpecification, String endpoint, int status){
+        Response response = requestSpecification.get(endpoint);
+        Assert.assertEquals(response.getStatusCode(),status);
+        response.then().log().all();
+        return response;
+    }
 }
